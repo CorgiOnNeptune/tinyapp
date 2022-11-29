4a1a85e9de77bbfd45fa915503519ad6ad4075f3
Justin <justinjdbennett@gmail.com>
2022-11-29 01:00:43 +0000

    Add a form that takes input for a POST request

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,9 +15,18 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
-  const templateVars = { id: req.params.id, longURL:  longURL};
+  const templateVars = { id: req.params.id, longURL: longURL};
   res.render('urls_show', templateVars);
 });
 
