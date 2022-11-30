3bec52dc37c51c6ae14c0a40c49481ca2ffd71eb
Justin <justinjdbennett@gmail.com>
2022-11-30 20:12:10 +0000

    Setup registration page

--- express_server.js
+++ express_server.js
@@ -49,6 +49,15 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 });
 
+app.get('/register', (req, res) => {
+  const templateVars = {
+    username: req.cookies.username,
+  };
+  
+  res.render('register', templateVars);
+});
+
+
 app.get('/urls', (req, res) => {
   // console.log('urlDatabase:\n', urlDatabase);
   const templateVars = {
